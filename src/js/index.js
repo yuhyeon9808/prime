181ce6895ceba32a30ebf6513f450bcc73e6bc08
yuhyeon9808 <bwj9808@gmail.com>
2026-01-12 19:19:50 +0900

    chore 반응형 수정 및 배너 수정

--- src/js/index.js
+++ src/js/index.js
@@ -12,9 +12,9 @@ const swiper = new Swiper('.main-slide', {
 
 // 시스템 슬라이드
 const systemSwiper = new Swiper('.system-slide', {
-  // autoplay: {
-  //   delay: 5000,
-  // },
+  autoplay: {
+    delay: 5000,
+  },
 
   navigation: {
     nextEl: '.system-next',
